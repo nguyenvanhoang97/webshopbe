a9f93df0013336c4b86a6e299682c0c03d7117c0
nguyenvanhoang97 <hoangnv180797@gmail.com>
2020-04-24 20:17:09 +0700

    11

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const helmet = require('helmet')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const api = require('../api')
 const start = (container) => {
@@ -20,11 +21,12 @@ const start = (container) => {
     app.use(morgan(':method :url :remote-addr :status :response-time ms - :res[content-length] :body - :req[content-length]'))
     app.use(bodyParser.urlencoded({ extended: false }))
     app.use(bodyParser.json())
-    // app.use(helmet())
-    // app.use(function (req, res, next) {
-    //   res.setHeader('Cross-Origin-Resource-Policy', 'same-site')
-    //   next()
-    // })
+    app.use(helmet())
+    app.use(cors())
+    app.use(function (req, res, next) {
+      res.setHeader('Cross-Origin-Resource-Policy', 'same-site')
+      next()
+    })
     app.use(verifyAccessToken)
     app.use((err, req, res, next) => {
       reject(new Error('Something went wrong!, err:' + err))
